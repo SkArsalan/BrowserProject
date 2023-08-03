90baeaaf6ca08489c4d8099e125ebdf8f203a20a
SkArsalan <98747542+SkArsalan@users.noreply.github.com>
2023-08-03 16:28:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const express = require('express');
-const { exec } = require('child_process');
+const puppeteer = require('puppeteer');
 
 const app = express();
 
-app.get('/get-page', (req, res) => {
+app.get('/render-page', async (req, res) => {
   const url = req.query.url; // Assuming you pass the URL as a query parameter
 
-  // Choose either w3m or lynx based on your preference
-  const command = `w3m -dump "${url}"`; // Or `lynx -dump "${url}"`
+  try {
+    const browser = await puppeteer.launch({
+      headless: true, // Set to false if you want to see the browser window
+      args: ['--no-sandbox', '--disable-setuid-sandbox'], // Required for running in AWS EC2 or similar environments
+    });
 
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing command: ${error.message}`);
-      return res.status(500).send('Error fetching the page.');
-    }
-    res.send(stdout);
-  });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' }); // Wait until the page finishes loading
+
+    // Optional: You can perform additional actions here, such as clicking buttons or filling forms
+
+    // Take a screenshot of the page
+    const screenshot = await page.screenshot({ fullPage: true });
+
+    await browser.close();
+
+    res.setHeader('Content-Type', 'image/png');
+    res.send(screenshot);
+  } catch (error) {
+    console.error(`Error rendering the page: ${error.message}`);
+    return res.status(500).send('Error rendering the page.');
+  }
 });
 
 const port = 3000;
